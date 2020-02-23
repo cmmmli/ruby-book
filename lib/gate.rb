b529c0295ab8f19f81194e5a2976c02fdda30f11
Yuichiro Komori <thekomori1113@gmail.com>
2020-02-23 20:38:43 +0900

    gate

---
+++ lib/gate.rb
@@ -0,0 +1,23 @@
+class Gate
+  STATIONS = %i(umeda juso mikuni)
+  FARES = [150, 190]
+
+  def initialize(name)
+    @name = name
+  end
+
+  def enter(ticket)
+    ticket.stamp(@name)
+  end
+
+  def exit(ticket)
+    ticket.fare >= calc_fare(ticket)
+  end
+
+  def calc_fare(ticket)
+    from = STATIONS.index(ticket.stamped_at)
+    to = STATIONS.index(@name)
+    distance = to - from
+    FARES[distance - 1]
+  end
+end
